efea940a312c9ba1e9cb7677f08f1cd3cd925b90
vikas kumar <vikaskumar1581997@gmail.com>
2022-06-22 23:06:13 +0530

    update2

--- src/Controllers/authorController.js
+++ src/Controllers/authorController.js
@@ -6,17 +6,30 @@ const createAuthor = async function (req, res) {
         const data = req.body
         console.log(data.firstName)
         console.log(typeof(data.firstName))
-        if((data.firstName==null)||(typeof(data.firstName)!==String)){
+        if(data.firstName==null){
             return res.status(400).send("first name should not be empty and should be string")}
+        
+        if(typeof(data.firstName)!="string"){
+            return res.status(400).send("first name should be string")}
         
         if(data.firstName.trim().length==0){
             return res.status(400).send("first name should not be empty ")}   
 
-        if(data.lastName==null||(typeof(data.firstName)!==String)){
+        if(data.lastName==null){
                 return res.status(400).send("last name should not be empty and should be string")}
             
          if(data.lastName.trim().length==0){
                 return res.status(400).send("last name should not be empty")}
+
+         if(data.password==null||(typeof(data.password)!==String)){
+                    return res.status(400).send("Password should not be empty and should be string")}
+                
+         if(data.password.trim().length==0){
+                    return res.status(400).send("password should not be blank")}
+         //========================================================           
+        const authorData=await authorModel.find()
+        if(data.password==authorData.password){  return res.status(400).send("password should be unique")}
+    
         console.log(data)
         const check = validator.validate(data.email)
         if(check){
